4d419b2d83bee1f5440ae4d23be7c099077618f0
public-jun <generalpublicjunjun@gmail.com>
2021-04-01 11:44:47 +0900

    add test.sh

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+for filepath in $SCRIPT_DIR/map_files/error*.cub; do
+	valgrind --leak-check=full --show-leak-kinds=all --errors-for-leak-kinds=all --error-exitcode=666 ./cub3D $filepath
+done
+
